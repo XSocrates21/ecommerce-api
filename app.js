46748cdce4ffaaded61f4a71a533f9e236c6979c
XSocrates21 <jeronarturo@outlook.com>
2023-11-04 20:46:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const categoriaRoutes = require('./routes/categoriaRoutes');
 const estadosRoutes = require('./routes/estadosRoutes');
 const marcasRoutes = require('./routes/marcasRoutes');
 const productosRoutes = require('./routes/productosRoutes');
-
+const rolesController = require('./routes/rolesRoutes');
+const usuariosController = require('./routes/usuariosRoutes');
+const ventasController = require('./routes/ventasRoutes');
 const app = express();
 
 app.use(cors());
@@ -15,6 +17,9 @@ app.use("/categoria", categoriaRoutes);
 app.use("/estados",estadosRoutes);
 app.use("/marcas",marcasRoutes);
 app.use("/productos",productosRoutes);
+app.use("/roles",rolesController);
+app.use("/usuarios",usuariosController);
+app.use("/ventas",ventasController);
 
 const PORT = 3001;
 app.listen(PORT, () => {
